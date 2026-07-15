454fc277271c8692eeb3411885ca8ef766b1db0a
elvishuges <elvishuges@hotmail.com>
2026-07-14 22:30:36 -0300

    adicionando metodos de mudancas no dominio

--- Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -1,14 +1,21 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
-import lombok.EqualsAndHashCode;
+import lombok.*;
+import org.hibernate.sql.ast.tree.expression.Collation;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter()
+@Getter
 public class Delivery {
 
     @EqualsAndHashCode.Include
@@ -33,4 +40,49 @@ public class Delivery {
     private ContactPoint recipient;
 
     private List<Item> items = new ArrayList<>();
+
+    public static Delivery draft() {
+        Delivery delivery = new Delivery();
+        delivery.setId(UUID.randomUUID());
+        delivery.setStatus( DeliveryStatus.DRAFT);
+        delivery.setTotalItems(0);
+        delivery.setTotalCost(BigDecimal.ZERO);
+        delivery.setCourierPayout(BigDecimal.ZERO);
+        delivery.setDistanceFee(BigDecimal.ZERO);
+        return delivery;
+    }
+
+    public UUID addItem(String name, int quantity) {
+        Item item = Item.brandNew(name, quantity);
+        items.add(item);
+        calculateTotalItems();
+        return item.getId();
+    }
+
+    public void removeItem(UUID itemId) {
+        items.removeIf(item -> item.getId().equals(itemId));
+        calculateTotalItems();
+    }
+
+    public void changeItemQuantity(UUID itemId, int quantity) {
+        Item item = getItems().stream().filter(i -> i.getId().equals(itemId))
+                .findFirst().orElseThrow();
+
+        item.setQuantity(quantity);
+        calculateTotalItems();
+    }
+
+    public void removeItems() {
+        items.clear();
+        calculateTotalItems();
+    }
+
+    public List<Item> getItems() {
+        return Collections.unmodifiableList(this.items);
+    }
+
+    private void calculateTotalItems() {
+        int totalItems = getItems().stream().mapToInt(Item::getQuantity).sum();
+        setTotalItems(totalItems);
+    }
 }
