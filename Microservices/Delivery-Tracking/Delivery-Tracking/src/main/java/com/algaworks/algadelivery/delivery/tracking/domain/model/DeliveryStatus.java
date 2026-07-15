8e7c49b9e615800c6b0a4ec2b7399c907236f43e
elvishuges <elvishuges@hotmail.com>
2026-07-15 15:04:17 -0300

    Adicionando persistência para Delivery

--- Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatus.java
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatus.java
@@ -7,7 +7,7 @@ public enum DeliveryStatus {
     DRAFT,
     WAITING_FOR_COURIER(DRAFT),
     IN_TRANSIT(WAITING_FOR_COURIER),
-    DELIVERY(IN_TRANSIT);
+    DELIVERED(IN_TRANSIT);
 
     private final List<DeliveryStatus> previousStatuses;
 
@@ -19,7 +19,7 @@ public enum DeliveryStatus {
         DeliveryStatus current = this;
         return !newStatus.previousStatuses.contains(current);
     }
-    
+
     public boolean canChangeTo(DeliveryStatus newStatus) {
         return !canNotChangeTo(newStatus);
     }
