43f67b9747b5b443304f53b3899e948e4ffcf0f5
elvishuges <elvishuges@hotmail.com>
2026-07-17 22:20:25 -0300

    adicionando servico e controller para Delivery

---
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -0,0 +1,32 @@
+package com.algaworks.algadelivery.delivery.tracking.api.controller;
+
+import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.service.DeliveryPreparationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/deliveries")
+@RequiredArgsConstructor
+public class DeliveryController {
+
+    private final DeliveryPreparationService deliveryPreparationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Delivery draft(@RequestBody @Valid DeliveryInput input) {
+        return deliveryPreparationService.draft(input);
+    }
+
+    @PutMapping("/{deliveryId}")
+    public Delivery edit(@PathVariable UUID deliveryId,
+            @RequestBody @Valid DeliveryInput input) {
+        return deliveryPreparationService.edit(deliveryId, input);
+    }
+
+}
